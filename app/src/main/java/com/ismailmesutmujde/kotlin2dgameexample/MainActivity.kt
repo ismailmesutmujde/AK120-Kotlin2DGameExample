2aa5a0264020596bc8aa47f4fbbc71d0a5ab3d7d
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-27 21:48:43 +0300

    initial commit

---
+++ app/src/main/java/com/ismailmesutmujde/kotlin2dgameexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ismailmesutmujde.kotlin2dgameexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
